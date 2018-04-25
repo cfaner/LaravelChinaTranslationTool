c03f4de93698d744822b00c6378fea578225ea60
dingmw <cfan@dingmw.cn>
2018-04-25 12:22:33 +0800

    增加功能 修复Bug

--- script/background.js
+++ script/background.js
@@ -1,5 +1,20 @@
 let appKey = APP_ID;
 let key = APP_SCRET; 
+ 
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {  
+	if (request.action == "get_translation_box_tpl")
+	{	
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", chrome.extension.getURL('/html/tpl/translation-box.html'), true);
+		xhr.onreadystatechange = function() {
+		  if (xhr.readyState == 4) {
+			sendResponse({result: xhr.responseText });
+		  }
+		};
+		xhr.send();
+		return true;
+	} 
+});
 
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {  
 	if (request.action == "get_net_translation_result")
@@ -31,6 +46,5 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 		});
 		
 		return true;
-	}
-	  
+	} 
 });
--- script/content.js
+++ script/content.js
@@ -1,197 +1,208 @@
 
-let currentSelectionWord = '';
+(function($, win , doc) {
+	
+	let currentSelectionWord = '';
 
-let words = {
-	data : {} ,
-	push : function( word )
-	{
-		if(word == '') return;
-		
-		let reg = /^[A-Za-z]+$/;
-		
-		if ( !reg.test( word ) )
+	let words = {
+		data : {} ,
+		push : function( word )
 		{
-			return;
+			if(isNotEnglishWord( word )) return;
+			
+			if( this.data [word] != undefined )
+			{
+				this.data [word] = this.data [word] + 1;
+			}
+			else
+			{
+				this.data [word] = 1;
+			}
+		},
+		result : function () {
+			return this.data;
 		}
+	};
 
-		if( this.data[word] != undefined )
+	let newWordDataListFromDatabase = database.getJSONItem('new_word');
+
+	let newWordDataList = {
+		word : []
+	}
+
+	if(newWordDataListFromDatabase)
+	{
+		newWordDataList = newWordDataListFromDatabase;
+	}
+
+	function inNewWordList(word)
+	{
+		return $.inArray( word , newWordDataList.word ) >= 0;
+	}
+
+	function isEnglishWord( word )
+	{
+		word = $.trim( word );
+		let reg = /^[A-Za-z]+$/;
+		return reg.test( word )
+	}
+
+	function isNotEnglishWord( word )
+	{
+		return !isEnglishWord( word );
+	}
+
+	function reg_replace( str , toStr , exp )
+	{
+		return str.replace( new RegExp( exp , 'g' ) , toStr );
+	}
+
+	function updateNewWord()
+	{
+		$(".translation-item > p").each(function(i,obj){
+			
+			let html = $(obj).html();
+			
+			html = reg_replace( html , "$1" , '<span.*?>(.*?)<\/span>'  );
+		 
+			for(let w of newWordDataList.word )
+			{
+				html = reg_replace( html ,  "$1<span style='color:red;font-weight:800;'>"+w+"</span>$2" , '([^A-Za-z0-9])'+w+'([^A-Za-z0-9])'  );
+			}
+			
+			$(obj).html(html);
+		});
+	}
+
+	updateNewWord();
+
+	function splitContentToWords( content )
+	{
+		content = content.replace("&nbsp;"," ");
+		
+		let c = content.split(" ");
+	 
+		for(let w of c )
 		{
-			this.data [word] = this.data [word] + 1;
+			words.push(w);
+		}
+	}
+
+	$(".translation-item > p").each(function(i,obj){
+		splitContentToWords( $(obj).text() );
+	});
+
+	document.addEventListener("dblclick", on_mouse_dbclick, true);
+
+	function writeTranslantionResultToHtml(result)
+	{
+		$("#translation-net-result").html("");
+		
+		if(result.basic != undefined && result.basic.explains != undefined)
+		{
+			$( result.basic.explains ).each(function(i,obj){
+				$("#translation-net-result").append( obj + "<br />" );
+			});
+		}
+		
+		if( result.translation != undefined)
+		{
+			$( result.translation ).each(function(i,obj){
+				$("#translation-net-result").append( obj + "<br />" );
+			});
+		}
+		
+	}
+
+	function getWordTranslationResult( word , callback )
+	{
+		let cacheTranslationResult = database.getJSONItem( 'word_cache_' + word.toLowerCase() );
+		
+		if( cacheTranslationResult )
+		{
+			callback( cacheTranslationResult ) ;
 		}
 		else
 		{
-			this.data [word] = 1;
+			chrome.runtime.sendMessage({action: "get_net_translation_result" , "word" : word }, function(response) {
+				console.log(response.result);
+				database.setJSONItem('word_cache_' + word.toLowerCase() , response.result );
+				callback( response.result ) ;
+			});
 		}
-	},
-	result : function () {
-		return this.data;
+
 	}
-};
 
-let newWordDataListFromDatabase = database.getJSONItem('new_word');
+	function showTranslationBox()
+	{
+		$("#translation-box").show();
+		$("#translation-box").css({
+			"left": event.pageX + "px",
+			"top":  ( event.pageY + 10 ) + "px",
+		});
+		
+		$("#translation-net-result").html("获取中,请稍后!");
+		getWordTranslationResult( currentSelectionWord ,function( result ){
+			writeTranslantionResultToHtml( result );
+		});
 
-let newWordDataList = {
-	word : []
-}
+		if( inNewWordList( currentSelectionWord ))
+		{
+			$("#btn-cancel-new-word").show();
+			$("#btn-new-word").hide();
+		}
+		else
+		{
+			$("#btn-cancel-new-word").hide();
+			$("#btn-new-word").show();
+		}
+	}
 
-if(newWordDataListFromDatabase)
-{
-	newWordDataList = newWordDataListFromDatabase;
-}
+	function on_mouse_dbclick(event) {
+		currentSelectionWord = $.trim( $.selection('text') ) ;
+		
+		if( isNotEnglishWord( currentSelectionWord )) {
+			currentSelectionWord = '';
+			return;
+		}
+		
+		showTranslationBox();
+	}
+	
+	chrome.runtime.sendMessage({ action: "get_translation_box_tpl" }, function(response) {
+		$("body").append( response.result );
+	
+		$("#btn-new-word").on('click',function(){
+			newWordDataList.word.push(currentSelectionWord);
+			database.setJSONItem("new_word",newWordDataList);
+			$("#translation-box").hide();
+			updateNewWord();
+		});
 
-function inNewWordList(word)
-{
-	return $.inArray( word , newWordDataList.word ) >= 0;
-}
-
-function isEnglishWord( word )
-{
-	let reg = /^[A-Za-z]+$/;
-	return reg.test( word )
-}
-
-function isNotEnglishWord( word )
-{
-	return !isEnglishWord( word );
-}
-
-function updateNewWord()
-{
-	$(".translation-item > p").each(function(i,obj){
+		$("#btn-cancel-new-word").on('click',function(){
+			
+			newWordDataList.word.splice($.inArray( currentSelectionWord , newWordDataList.word ),1);
+			
+			database.setJSONItem("new_word",newWordDataList);
+			
+			$("#translation-box").hide();
+			
+			updateNewWord();
+		});
+		
+		$("#btn-close").on('click',function(){
+			$("#translation-box").hide();
+		});
+	});
+	
+	$(".CodeMirror-line").each(function(i,obj){
 		
 		let html = $(obj).html();
 		
-		let exp = new RegExp( '<span.*?>(.*?)<\/span>' , 'g' );
-		html = html.replace( exp , "$1");
+		html.replace('\,','，');
+		html.replace('\.','。');
+		html.replace('response','。');
 
-		for(let w of newWordDataList.word )
-		{
-			let exp = new RegExp( '([^A-Za-z0-9])'+w+'([^A-Za-z0-9])' , 'g' );
-			html = html.replace( exp , "$1<span style='color:red;font-weight:800;'>"+w+"</span>$2");
-		}
-		
-		$(obj).html(html);
-	});
-}
-
-updateNewWord();
-
-function splitContentToWords( content )
-{
-	content = content.replace("&nbsp;"," ");
-	
-	let c = content.split(" ");
- 
-	for(let w of c )
-	{
-		words.push(w);
-	}
-}
-
-$(".translation-item > p").each(function(i,obj){
-	splitContentToWords( $(obj).text() );
-});
-
-document.addEventListener("dblclick", on_mouse_dbclick, true);
-
-function writeTranslantionResultToHtml(result)
-{
-	$("#translation-net-result").html("");
-	
-	if(result.basic != undefined && result.basic.explains != undefined)
-	{
-		$( result.basic.explains ).each(function(i,obj){
-			$("#translation-net-result").append( obj + "<br />" );
-		});
-	}
-	
-	if( result.translation != undefined)
-	{
-		$( result.translation ).each(function(i,obj){
-			$("#translation-net-result").append( obj + "<br />" );
-		});
-	}
-	
-}
-
-function getWordTranslationResult( word , callback )
-{
-	let cacheTranslationResult = database.getJSONItem( 'word_cache_' + word.toLowerCase() );
-	
-	if( cacheTranslationResult )
-	{
-		callback( cacheTranslationResult ) ;
-	}
-	else
-	{
-		chrome.runtime.sendMessage({action: "get_net_translation_result" , "word" : word }, function(response) {
-			console.log(response.result);
-			database.setJSONItem('word_cache_' + word.toLowerCase() , response.result );
-			callback( response.result ) ;
-		});
-	}
-
-}
-
-function on_mouse_dbclick(event) {
-	currentSelectionWord = $.trim( $.selection('text') ) ;
-	
-	if( isNotEnglishWord( currentSelectionWord )) {
-		currentSelectionWord = '';
-		return;
-	}
-	
-	$("#translation-box").show();
-	$("#translation-box").css({
-		"left": event.pageX + "px",
-		"top":  ( event.pageY + 10 ) + "px",
+		//$(obj).html("test");
 	});
 	
-	$("#translation-net-result").html("获取中,请稍后!");
-	getWordTranslationResult( currentSelectionWord ,function( result ){
-		writeTranslantionResultToHtml( result );
-	});
-
-	if( inNewWordList( currentSelectionWord ))
-	{
-		$("#btn-cancel-new-word").show();
-		$("#btn-new-word").hide();
-	}
-	else
-	{
-		$("#btn-cancel-new-word").hide();
-		$("#btn-new-word").show();
-	}
-}
-
-$("body").append('<div id="translation-box"><a href="javascript:;" id="btn-cancel-new-word" style="display:none;">取消</a><a href="javascript:;" id="btn-new-word">生词</a><div id="translation-net-result"></div></div>');
-
-$("#translation-box").css({
-	"position":"absolute",
-	"background-color":"#FFF",
-	"left":"0px",
-	"top":"0px",
-	"border":"1px solid #d3e0e9",
-	"padding":"15px",
-	"display":"none",
-	"-webkit-box-shadow":"0px 1px 2px 0 rgba(101, 129, 156, 0.08)",
-	"box-shadow":"0px 1px 2px 0 rgba(101, 129, 156, 0.08)"
-});
-
-$("#btn-new-word").on('click',function(){
-	newWordDataList.word.push(currentSelectionWord);
-	database.setJSONItem("new_word",newWordDataList);
-	$("#translation-box").hide();
-	updateNewWord();
-});
-
-$("#btn-cancel-new-word").on('click',function(){
-	
-	newWordDataList.word.splice($.inArray( currentSelectionWord , newWordDataList.word ),1);
-	
-	database.setJSONItem("new_word",newWordDataList);
-	
-	$("#translation-box").hide();
-	
-	updateNewWord();
-});
+})(jQuery, window , window.document);
